e2bff8640f63da4e6100e5a8603678809441134d
Kamil Nyasibullin <jjensen8357@gmail.com>
2019-09-15 02:28:13 +0300

    Изменен метод конвертации url в маршут, исправлен баг, который выдавал ошибку 404, если вы заходили с illuin.ru (без /index.php), изменена система создания массива , теперь она проверяет наличие того или иного ключа в глобальном массиве

--- core/Router.php
+++ core/Router.php
@@ -13,46 +13,59 @@ class Router
             $this->routes = require_once 'config/routes.php'; //здесь содержимое файла routes.php копируется в переменную routes класса Router
     }
 
-    private function formating_patterns($pattern){ //фукнция, которая превращает ключи массива routes в регулярные выражения, то есть шаблоны
+    private function convert_to_regex($pattern){ //фукнция, которая превращает ключи массива routes в регулярные
+        // выражения, то
+        // есть
+        // шаблоны
         $pattern = '#^' . $pattern . '$#';
         return $pattern;
     }
 
     private function match_single_pattern($pattern, $potential_route){ //фукнция осуществляющая проверку потенциаольного шаблона, шаблоном из массива routes
-        return preg_match($this->formating_patterns($pattern), $potential_route);
+        return preg_match($this->convert_to_regex($pattern), $potential_route);
     }
 
     private function get_request_path($url){ //получение URI, который потребовался для доступа к данной странице
-        return (trim($url));
+        return trim(htmlspecialchars($url));
+    }
+
+    private function convert_url_to_route($url){
+        $potential_route = substr(strpos($url, '.')  ? strstr($url, '.', true) : $url, 1); //в переменной будет либо
+        // пустота, либо название файла
+        if(stristr($potential_route, '?')){
+            $potential_route = substr($potential_route, 0, strpos($potential_route, '?'));
+        } //всегда убираем ? ибо он нам не нужен
+
+        return $potential_route;
     }
 
     private function parseUrl($url){ //сравнение возможного шаблона со всеми возможными шшаблонами из массива routes
         $potential_route = $this->get_request_path($url); //получение URI
-        $potential_route = substr(strpos($potential_route, '.')  ? strstr($potential_route, '.', true) : $potential_route, 1); //в переменной будет хранится либо пустота, либо название файла
-        if(strpos($potential_route, '?')) $potential_route = substr($potential_route, 0, strpos($potential_route, '?')); //всегда убираем ? ибо он нам не нужен
+        $potential_route = $this->convert_url_to_route($potential_route); //оставим только возможный маршут
         //TODO добавить в качестве сравнения регулярные выражения
         foreach($this->routes as $pattern => $contr_action){ //сравнение шаблонов
             if($this->match_single_pattern($pattern, $potential_route)){
-                return $contr_action; //в случае успеха вернуть контроллер-метод этого шаблона
+                return $contr_action; //в случае успеха вернуть контроллер-акшон этого шаблона
             }
         }
         return false; //иначе - false
     }
     private function parse_params($params = []){ //функция принимает в качестве аргумента GET-параметр, который превращается в число и возращает это число
         if(isset($params)) {
-            $count_params = count($params);
-            if($count_params === 1){ //если параметр один, то это id статьи, и нужно в $params засунуть id статьи
-                if(isset($_REQUEST['id'])) {
-                    $id = (int)$_REQUEST['id'];
-                    $params = ['id' => $id];
-                }
-            }elseif($count_params === 2 and !empty($params['email']) and !empty($params['password'])){//если параметра два, то это логин и пароль, их нужно засунуть в $params
-                    $params = ['email' => htmlspecialchars($_REQUEST['email']), 'password' => crypt(htmlspecialchars($_REQUEST['password']), htmlspecialchars($_REQUEST['email']))];
-            }elseif($count_params === 2 and !empty($params['title']) and !empty($params['text'])){
-              $params = ['title' => $_REQUEST['title'], 'text' => $_REQUEST['text']] ;
-            } elseif($count_params === 3 and !empty($params['emai']) and !empty($params['author_name']) and !empty($params['password'])){//если параметра три, то это логин, пароль и имя автора (никнейм), их тоже нужно засунуть в $params
-                $params = ['author_name' => htmlspecialchars($_REQUEST['author_name']), 'email' => htmlspecialchars($_REQUEST['email']), 'password' => crypt(htmlspecialchars($_REQUEST['password']), htmlspecialchars($_REQUEST['email']))];
+            if(array_key_exists('id', $params)){ //если параметр один, то это id статьи, и нужно в $params засунуть id статьи
+                if(isset($_GET['id'])) $params = ['id' => (int)$_REQUEST['id']];
+            }elseif(array_key_exists('email', $params) and array_key_exists('password', $params) and array_key_exists
+                ('author_name', $params)){//если параметра три, то это логин, пароль и имя автора (никнейм), их тоже
+                // нужно засунуть в $params
+                if(isset($_POST['email']) and isset($_POST['password']) and isset($_POST['author_name']))$params = ['author_name' => htmlspecialchars($_REQUEST['author_name']), 'email' => htmlspecialchars($_REQUEST['email']), 'password' => crypt(htmlspecialchars($_REQUEST['password']), htmlspecialchars($_REQUEST['email']))];
+            }elseif(array_key_exists('email', $params) and array_key_exists('password', $params)){//если параметра два, то это логин и пароль, их нужно засунуть в $params
+                if(isset($_POST['email']) and isset($_POST['password'])) $params = ['email' => htmlspecialchars($_REQUEST['email']), 'password' => crypt(htmlspecialchars($_REQUEST['password']), htmlspecialchars($_REQUEST['email']))];
+            }elseif(array_key_exists('title', $params) and array_key_exists('text', $params)){
+              if(isset($_POST['title']) and isset($_POST['text'])) $params = ['title' => $_POST['title'], 'text' => $_POST['text']];
+            }else{
+                $params = [];
             }
+
         }
 //            $get_param = (int) $get_param;
         return $params;
@@ -63,12 +76,11 @@ class Router
     }
 
     public function run(){ //запуск обработки
-        $result = $this->parseUrl($_SERVER['REQUEST_URI']); //результат сравнения или преобразования url в шаблон
+        $route = $this->parseUrl($_SERVER['REQUEST_URI']); //результат сравнения или преобразования url в шаблон
         $params = $this->parse_params($_REQUEST); //отправляем на обработку весь массив
-//        @$params = (is_null ($this->parse_get_param($_GET['id']))) ? '' : ['id' => $this->parse_get_param($_GET['id'])]; //id-статьи. Идет проверка на нулевость парамтра, если он нулевой то в $params будет присвоена пустая строка, иначе будет присвоен id
-        if(!empty($params['PHPSESSID'])) unset($params['PHPSESSID']);
-        if($result){ //если вернуло контроллер-метод
-            $contr_action = explode('/', $result); //делим строку контроллер/метод на массив в котором содержиться под индексом 0 - контроллер, под индексом 1 - метод
+        if($route){ //если вернуло контроллер-метод
+            $contr_action = explode('/', $route); //делим строку контроллер/метод на массив в котором содержиться под
+            // индексом 0 - контроллер, под индексом 1 - метод
             //перменная $cont_action - это массив ['контроллер', ['действие_контроллера']
             $contr_action = array_combine(['controller', 'action'], $contr_action); // превращаем массив с числовыми индексами в массив с строковыми индексами (чтобыб понятнее было)
             $controller_name = 'controllers\\' .  ucfirst($contr_action['controller']) . 'Controller'; //Формируем имя контроллера с названия пространсва имен,  большой буквы и префиксом Controlller
